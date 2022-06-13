a9b5dc32a7755f95adc67fa03fd38402374b21f5
David Basto <davidbasto01@gmail.com>
2022-06-12 22:41:18 -0500

    [ADD] linters config

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Sales Record App</title>
+        <meta name="description" content="Sales record app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </>
+  );
+};
+
+export default Home;
